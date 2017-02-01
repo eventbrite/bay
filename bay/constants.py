5a8fbf4d44d6cb0616f247e575cf1c96d4c7fa42
Andrew Godwin <andrew@aeracode.org>
2017-01-31 16:43:20 -0800

    Catch docker connection errors nicely and add hook for nicer errors

--- bay/constants.py
+++ bay/constants.py
@@ -3,5 +3,12 @@ class PluginHook:
     POST_BUILD = "post-build"
     PRE_START = "pre-start"
     POST_START = "post-start"
+    DOCKER_FAILURE = "docker-fail"
 
-    valid_hooks = frozenset([PRE_BUILD, POST_BUILD, PRE_START, POST_START])
+    valid_hooks = frozenset([
+        PRE_BUILD,
+        POST_BUILD,
+        PRE_START,
+        POST_START,
+        DOCKER_FAILURE,
+    ])
